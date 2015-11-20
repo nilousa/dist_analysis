c49e72ebbf0e1722684b8d8cf8b74f591c7bf681
nilousa <nilousa@Mac-Pro.local>
2015-11-20 10:46:43 -0500

    f

--- dist_analysis.py
+++ dist_analysis.py
@@ -6,11 +6,12 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-xml = input('xml: ')
-dcd = input('dcd: ')
+#xml = input('xml: ')
+#dcd = input('dcd: ')
 
 u = mda.Universe(xml,dcd)
 dij = []
+x = u.selectAtoms('type B or type C or type S or type Z or type N')[9::19]
 for ts in u.trajectory:
     dij.append(self_distance_array(x.positions))
 dij = np.array(dij).T
